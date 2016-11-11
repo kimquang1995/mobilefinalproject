82628a647b923c52421b513e30ec8dbeb9b9ff75
kimquang1995 <kimquang1995@gmail.com>
2016-11-11 11:33:23 +0700

    First Update
    Create Project

---
+++ ATSM_WMP_FinalProject/app/src/androidTest/java/mobile/atsm/atsm_wmp_finalproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package mobile.atsm.atsm_wmp_finalproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
