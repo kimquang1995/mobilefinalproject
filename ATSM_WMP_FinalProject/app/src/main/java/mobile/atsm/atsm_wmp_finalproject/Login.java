e98d6929aab9edf7a297318a2f25e1e2191bfedf
kimquang1995 <kimquang1995@gmail.com>
2016-12-21 22:47:18 -0800

    Login and SignUp complete

--- ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/Login.java
+++ ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/Login.java
@@ -1,5 +1,6 @@
 package mobile.atsm.atsm_wmp_finalproject;
 
+import android.content.Entity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,19 +13,41 @@ import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import mobile.atsm.atsm_wmp_finalproject.Adapter.MyFunctions;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Login extends AppCompatActivity {
-RelativeLayout acitivitymain;
-    String sMail,sPass;
+    RelativeLayout acitivitymain;
+    String sMail, sPass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_login);
         // Set background
-        acitivitymain = (RelativeLayout)findViewById(R.id.activity_main);
+        acitivitymain = (RelativeLayout) findViewById(R.id.activity_main);
         acitivitymain.setBackgroundResource(R.drawable.background2);
 
         final EditText etEmail = (EditText) findViewById(R.id.edtEmail);
@@ -33,10 +56,13 @@ RelativeLayout acitivitymain;
             @Override
             public void onClick(View v) {
                 try {
-                    sMail=etEmail.getText().toString();;
-                    sPass=etPass.getText().toString();
-                    new thucthilogin().execute();
-
+                    sMail = etEmail.getText().toString();
+                    sPass = etPass.getText().toString();
+                    if (sMail.length() >0 && sPass.length()>0) {
+                        new exe_Login().execute("http://www.stsmteam.esy.es/index.php", sMail, sPass);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Please Input Email or Password", Toast.LENGTH_SHORT).show();
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -45,60 +71,70 @@ RelativeLayout acitivitymain;
         findViewById(R.id.btnSignup).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Login.this,ViewTasks.class);
+                Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-
-
     }
-    class thucthilogin extends AsyncTask<Void,Void,String>
-    {
-        MyFunctions myfunctions;
-        String email;
-        String password;
+
+    class exe_Login extends AsyncTask<String, Integer, String> {
+
         @Override
-        protected String doInBackground(Void... arg0) {
-            // TODO Auto-generated method stub
-            email=sMail;
-            password=sPass;
-            String thanhcong=null;
-
-            try{
-                myfunctions=new MyFunctions(getApplicationContext());
-                JSONObject jsonobject=myfunctions.loginUser(email, password);
-
-                thanhcong=jsonobject.getString("thanhcong");
-
-            }catch(Exception e)
-            {
-                Log.d("loi", "khong tao json duoc "+ e.toString());
-            }
-            return thanhcong;
+        protected String doInBackground(String... params) {
+            return makeLogin(params[0],params[1],params[2]);
         }
 
         @Override
-        protected void onPostExecute(String thanhcong) {
-            // TODO Auto-generated method stub
-            super.onPostExecute(thanhcong);
-            if(Integer.parseInt(thanhcong)==1) //dang nhap thanh cong
-            {
-                myfunctions.setemaillogin(email);//luu mail lai
-                Intent i=new Intent(getApplicationContext(),AddTag.class);
-                startActivity(i);
-                finish();
-            }
-            else //dang nhap that bai
-            {
-                Toast.makeText(getApplicationContext(),
-                        "khong dang nhap duoc email hoac pass sai",
-                        Toast.LENGTH_SHORT).show();
+        protected void onPostExecute(String s) {
+          //  Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            try {
+               JSONObject object = new JSONObject(s);
+                if (Integer.parseInt(object.getString("thanhcong"))==1)
+                {
+                    Intent intent = new Intent(Login.this, ViewTags.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Email or Password is wrong", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
         }
-
     }
+
+    private String makeLogin(String url,String email,String password) {
+        HttpClient httpClient = new DefaultHttpClient();
+        // URL cua trang nhan Request
+        HttpPost httpPost = new HttpPost(url);
+        //Cac tham so truyen
+        List nameValuePair = new ArrayList(3);
+        nameValuePair.add(new BasicNameValuePair("tag", "login"));
+        nameValuePair.add(new BasicNameValuePair("email", email));
+        nameValuePair.add(new BasicNameValuePair("password", password));
+        //Encode Post data
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String kq = "";
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            kq = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return kq;
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/SignUp.java
+++ ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/SignUp.java
@@ -1,13 +1,123 @@
 package mobile.atsm.atsm_wmp_finalproject;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SignUp extends AppCompatActivity {
-
+String email,pass,name;
+    EditText etEmai,etPass,etName;
+    TextView tvStatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        etEmai= (EditText) findViewById(R.id.edtInputEmail);
+        etPass= (EditText) findViewById(R.id.edtInputPass);
+        etName= (EditText) findViewById(R.id.edtInputName);
+        tvStatus= (TextView) findViewById(R.id.txtStatus);
+        findViewById(R.id.btnCancelSignUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        findViewById(R.id.btnSignUp).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email=etEmai.getText().toString().trim();
+                name=etName.getText().toString().trim();
+                pass=etPass.getText().toString().trim();
+                if(email.length()>0 && name.length()>0 && pass.length()>0) {
+                    new exeSignUp().execute("http://www.stsmteam.esy.es/index.php", name, email, pass);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),"Please input fully", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+    class exeSignUp extends AsyncTask<String,Integer,String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+           return makeSignUp(params[0],params[1],params[2],params[3]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+          //  Toast.makeText(getApplicationContext(),s, Toast.LENGTH_SHORT).show();
+            if(s.trim().equalsIgnoreCase("success"))
+            {
+                tvStatus.setTextColor(Color.BLUE);
+                tvStatus.setText("Congurations ! Register Successfully ");
+
+            }
+            else if(s.trim().equals("email is used"))
+            {
+                tvStatus.setTextColor(Color.RED);
+                tvStatus.setText("Failure ! Email was used,Please Input another one ");
+
+            }
+            else
+            {
+                tvStatus.setTextColor(Color.RED);
+                tvStatus.setText("Have Error ! Please try again");
+            }
+        }
+    }
+    private String makeSignUp(String url,String name,String email,String password) {
+        HttpClient httpClient = new DefaultHttpClient();
+        // URL cua trang nhan Request
+        HttpPost httpPost = new HttpPost(url);
+        //Cac tham so truyen
+        List nameValuePair = new ArrayList(3);
+        nameValuePair.add(new BasicNameValuePair("tag", "register"));
+        nameValuePair.add(new BasicNameValuePair("name", name));
+        nameValuePair.add(new BasicNameValuePair("email", email));
+        nameValuePair.add(new BasicNameValuePair("password", password));
+        //Encode Post data
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String kq = "";
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            kq = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return kq;
+    }
+
 }
