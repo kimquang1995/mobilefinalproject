cfb191fbd51a1930b6c7c9bd92384af80843d791
lehoanggiang <lehoanggiang1995@gmail.com>
2016-12-26 10:15:39 +0700

    fix GUI and doing Send Mail contain password

--- ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/GetPassword.java
+++ ATSM_WMP_FinalProject/app/src/main/java/mobile/atsm/atsm_wmp_finalproject/GetPassword.java
@@ -1,45 +1,93 @@
 package mobile.atsm.atsm_wmp_finalproject;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 import android.app.Activity;
-import android.app.PendingIntent;
-import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.telephony.SmsManager;
-import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-public class GetPassword extends Activity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+
+public class GetPassword extends Activity
+{
+    public String jobNo;
+    public String teamNo;
+    private static final String username = "atsmteamgmail.com";
+    private static final String password = "p123456p";
+    private static  String emailid ="";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_password);
-        findViewById(R.id.btnCancel).setOnClickListener(new View.OnClickListener() {
+        final EditText edtMail =(EditText) findViewById(R.id.edtEmail);
+      //  Intent intent = getIntent();
+       // jobNo = intent.getStringExtra("Job_No");
+      //  teamNo = intent.getStringExtra("Team_No");
+
+        findViewById(R.id.btnSendMail).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(GetPassword.this, Login.class);
-                startActivity(intent);
-                finish();
+                emailid = edtMail.getText().toString().trim();
+                new exeGetPass().execute("http://stsmteam.esy.es/getpass.php",emailid);
             }
         });
-        findViewById(R.id.btnSendPhone).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
-                emailIntent.setData(Uri.parse("mailto:" + "recipient@example.com"));
-                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "My email's subject");
-                emailIntent.putExtra(Intent.EXTRA_TEXT, "My email's body");
 
-                try {
-                    startActivity(Intent.createChooser(emailIntent, "Send email using..."));
-                } catch (android.content.ActivityNotFoundException ex) {
-                    Toast.makeText(GetPassword.this, "No email clients installed.", Toast.LENGTH_SHORT).show();
-                }
+    }
 
-            }
-        });
+    class exeGetPass extends AsyncTask<String, Integer,String>
+    {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return getPass(params[0],params[1]);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(GetPassword.this,"Your Password is "+s.trim(),Toast.LENGTH_SHORT).show();
+        }
+    }
+    private String getPass(String url, String email) {
+        HttpClient httpClient = new DefaultHttpClient();
+        // URL cua trang nhan Request
+        HttpPost httpPost = new HttpPost(url);
+        //Cac tham so truyen
+        List nameValuePair = new ArrayList(2);
+        nameValuePair.add(new BasicNameValuePair("email", email));
+        //Encode Post data
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        String kq = "";
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            HttpEntity entity = response.getEntity();
+            kq = EntityUtils.toString(entity);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return kq;
     }
 }
